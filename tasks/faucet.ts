894bff9d909c17bc436fde2fe624a023d41f9c4f
Eddie Ho <boho@ucsd.edu>
2023-12-29 16:27:47 -0800

    ⚒️ draft mortgage module (need attentions)

--- tasks/faucet.ts
+++ tasks/faucet.ts
@@ -2,12 +2,13 @@ import { task } from "hardhat/config";
 
 task("faucet", "Sends ETH to an address")
   .addPositionalParam("receiver", "The address that will receive them")
-  .setAction(async ({ receiver }, { ethers }) => {
+  .addPositionalParam("amount", "The amount of ETH to send")
+  .setAction(async ({ receiver, amount }, { ethers }) => {
     const [sender] = await ethers.getSigners();
     const tx = await sender.sendTransaction({
       to: receiver,
-      value: ethers.WeiPerEther,
+      value: ethers.toBigInt(parseInt(amount)) * ethers.WeiPerEther,
     });
     await tx.wait();
-    console.log(`Transferred 1.0 ETH to ${receiver}`);
+    console.log(`Transferred ${amount} ETH to ${receiver}`);
   });
