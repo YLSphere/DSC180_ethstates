166655cc3b7d6462e2b2f026cc18ecdc21508d6d
Chaohao Du <chaohaodu@gmail.com>
2024-05-07 17:08:49 -0700

    complete the investor form

--- frontend/src/pages/Investors.tsx
+++ frontend/src/pages/Investors.tsx
@@ -1,35 +1,50 @@
 import {
-  Badge,
   Container,
-  Heading,
   Spinner,
-  FormControl,
-  FormLabel,
-  Input,
-  SimpleGrid,
   Alert,
   AlertDescription,
   AlertIcon,
   AlertTitle,
+  FormControl,
+  FormLabel,
+  Button,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper, NumberDecrementStepper,
 } from "@chakra-ui/react";
-import NftCard from "../components/property/NftCard";
-import NftCollection from "../components/property/NftCollection";
 import { useGetAllListings } from "../hooks/marketplace/useListing";
 import { useAccount } from "wagmi";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Nft } from "../types/listing";
 import { CHAIN_ID } from "../types/constant";
+
 
 export default function Investors() {
   const { address, chain, isConnected } = useAccount();
   const [nfts, setNfts] = useState<Nft[] | undefined>([]);
   const { isLoading, data } = useGetAllListings(address);
+  const [rate, setRate] = useState<number>(5);
+  const [durationMonth, setDurationMonth] = useState<number>(12);
 
   useEffect(() => {
     if (!isLoading) {
       setNfts(data);
     }
   }, [isLoading, data]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // todo
+  }
+
+    const formatRate = (value:number)=> {
+        return value + '%';
+    }
+
+    const parseRate = (valueString: string) => {
+        return parseFloat(valueString.replace("%", ""))
+    }
 
   // Wrong network
   if (isConnected && chain?.id !== CHAIN_ID) {
@@ -81,22 +96,50 @@ export default function Investors() {
     );
   }
 
+
   // NFTs are loaded
   return (
-    <main>
-      <Container maxWidth="container.lg" my={5}>
-        <Heading as="h1" size="xl" mt={8}>
-          Investors{" "}
-          <Badge
-            borderRadius="full"
-            fontSize="x-large"
-            px="2"
-            colorScheme="green"
-          >
-            {nfts?.length}
-          </Badge>
-        </Heading>
-      </Container>
-    </main>
+      <main>
+        <Container maxWidth={"container.md"}>
+          <form onSubmit={handleSubmit}>
+            <FormControl id="rate" isRequired mt={3}>
+              <FormLabel>Annual interest rate</FormLabel>
+              <NumberInput
+                  onChange={(valueString) => setRate(parseRate(valueString))}
+                  value={formatRate(rate)}
+                  min={0}
+                  max={100}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+
+            <FormControl id="duration" isRequired mt={3}>
+              <FormLabel>Max Duration in Month</FormLabel>
+              <NumberInput
+                  defaultValue={12}
+                  precision={0}
+                  max={360}
+                  min={12}
+                  onChange={(value)=>setDurationMonth(parseInt(value))}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </FormControl>
+
+            <Button my={4} colorScheme="teal" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Container>
+      </main>
   );
 }
